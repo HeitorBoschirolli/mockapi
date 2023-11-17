1bae3afc4bbdbd9c81e6883d032b550ab837b9ab
Heitor Boschirolli Comel <heitor.boschirolli@gmail.com>
2023-11-16 21:55:27 -0300

    Create simple API mock

--- ApiMock/Program.cs
+++ ApiMock/Program.cs
@@ -8,27 +8,13 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/api", (HttpRequest request) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return new ResponseDto(request.Query["id"]);
 });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+internal record ResponseDto(string Id)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
